15046da9f0ad5905ef7b2333f95e100ab58b8cfd
Sasha <aleksandr.zapashniy@mail.ru>
2021-12-18 17:56:32 +0300

    feat(): create project

---
+++ app/src/main/java/ru/zapashnii/rickandmorty/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.zapashnii.rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ru.zapashnii.rickandmorty.ui.theme.RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
