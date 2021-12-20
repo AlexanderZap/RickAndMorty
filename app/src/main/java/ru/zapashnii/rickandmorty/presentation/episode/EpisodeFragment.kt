e56fbbce0353823924a18eda32e132ea2198b579
Sasha <aleksandr.zapashniy@mail.ru>
2021-12-20 21:31:22 +0300

    feat(Episode): create Episode screen
    - Создан экран Экран эпизодов

---
+++ app/src/main/java/ru/zapashnii/rickandmorty/presentation/episode/EpisodeFragment.kt
@@ -0,0 +1,33 @@
+package ru.zapashnii.rickandmorty.presentation.episode
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.zapashnii.rickandmorty.R
+
+/** Экран эпизодов */
+class EpisodeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EpisodeFragment()
+    }
+
+    private lateinit var viewModel: EpisodeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.episode_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EpisodeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/zapashnii/rickandmorty/presentation/episode/EpisodeViewModel.kt
@@ -0,0 +1,7 @@
+package ru.zapashnii.rickandmorty.presentation.episode
+
+import androidx.lifecycle.ViewModel
+
+class EpisodeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
